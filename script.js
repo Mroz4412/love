94fad491eabbd5cdb97c036971dd9737c09e8c89
Mroz4412 <quba.mroz1@gmail.com>
2025-03-27 19:24:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const textElement = document.querySelector(".text");
+const text = document.getElementById("loveText");
 
-    const animation = new mojs.Timeline().add([
-        new mojs.Tween({
-            duration: 1000,
-            easing: "sin.inOut",
-            onUpdate: function (progress) {
-                textElement.style.transform = `scale(${1 + progress * 0.5})`;
-                textElement.style.opacity = progress;
-            },
-        })
-    ]);
+const animation = new mojs.Timeline().add(
+    new mojs.Html({
+        el: text,
+        duration: 1000,
+        opacity: { 0: 1 },
+        scale: { 0.5: 1 },
+        easing: "sin.out",
+    })
+);
 
-    animation.play();
-});
+animation.play();
